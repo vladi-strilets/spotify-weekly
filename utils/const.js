d799ae938ac7c2c23331828f82a12349ff230769
Vladimir Strilets <vladi.strilets@gmail.com>
2020-10-18 15:05:26 +0200

    url fix

--- utils/const.js
+++ utils/const.js
@@ -1,4 +1,8 @@
-export const REDIRECT_URI =
+const REDIRECT_URI =
 	!process.env.NODE_ENV || process.env.NODE_ENV === "development"
 		? "http://localhost:5000/"
 		: "https://spotify-weekly.herokuapp.com/";
+
+module.exports = {
+	REDIRECT_URI,
+};

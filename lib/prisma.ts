1c09f379832201d3378d79e6ab1ab225efc445b5
Vladimir Strilets <vladi.strilets@gmail.com>
2022-12-24 21:22:52 +0000

    feat: use prisma from generated folder

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,9 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../supabase/functions/generated/client";
 
-const prisma = global.prisma || new PrismaClient();
+// const prisma = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
+const prisma = new PrismaClient();
 
 export default prisma;
